592ce4ec71f7c7269cba86931f10464d23954fb5
dev1 <SatyaDev1@gmail.com>
2020-04-10 15:56:21 +0530

    commit TL

--- DemoApp/src/com/core/beans/DateClasssDemo.java
+++ DemoApp/src/com/core/beans/DateClasssDemo.java
@@ -3,6 +3,7 @@ package com.core.beans;
 public class DateClasssDemo {
     public static void main(String[] args) {
     	System.out.println("hii");
+    	System.out.println("DateClasssDemo.main()");
 		}
 	}
 
