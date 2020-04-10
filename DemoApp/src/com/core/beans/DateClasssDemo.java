7a48aeb2ac1dc812ba188baf5a418b6259ddebb9
dev1 <SatyaDev1@gmail.com>
2020-04-10 15:46:21 +0530

    inital commit by TL

---
+++ DemoApp/src/com/core/beans/DateClasssDemo.java
@@ -0,0 +1,8 @@
+package com.core.beans;
+
+public class DateClasssDemo {
+    public static void main(String[] args) {
+    	System.out.println("hii");
+		}
+	}
+
---
+++ DemoApp/src/com/core/test/TestClass.java
@@ -0,0 +1,8 @@
+package com.core.test;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
